c0a4e45e1424ed60808bfc3098ce4c8308513b96
karenaylinfdz <karenaylinfdz45@gmail.com>
2025-10-29 10:30:28 -0500

    Add files via upload

---
+++ U3-1 Practica 17/U3-1 Practica 17/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace U3_1_Practica_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(182, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(548, 34);
+            label1.TabIndex = 0;
+            label1.Text = "Ordenar 3 numeros de mayor a menor ";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(285, 148);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(401, 148);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(521, 148);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label2.Location = new Point(320, 185);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 22);
+            label2.TabIndex = 4;
+            label2.Text = "A";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label3.Location = new Point(438, 185);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 22);
+            label3.TabIndex = 5;
+            label3.Text = "B";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label4.Location = new Point(563, 185);
+            label4.Name = "label4";
+            label4.Size = new Size(24, 22);
+            label4.TabIndex = 6;
+            label4.Text = "C";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button1.Location = new Point(297, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 7;
+            button1.Text = "Ordenar";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(546, 253);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 8;
+            button2.Text = "Salir";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button3.Location = new Point(417, 253);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Borrar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(285, 328);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(401, 328);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 23);
+            textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(521, 328);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 23);
+            textBox6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Moccasin;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+    }
+}
