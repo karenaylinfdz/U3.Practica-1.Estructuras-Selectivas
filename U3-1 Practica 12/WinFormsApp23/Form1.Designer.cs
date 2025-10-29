c9c29d82be62792279b7f542de1d3e79b69034e0
karenaylinfdz <karenaylinfdz45@gmail.com>
2025-10-29 10:23:16 -0500

    Add files via upload

---
+++ U3-1 Practica 12/WinFormsApp23/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WinFormsApp23
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(120, 155);
+            label1.Name = "label1";
+            label1.Size = new Size(211, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Calificacion:";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(272, 263);
+            button1.Name = "button1";
+            button1.Size = new Size(133, 53);
+            button1.TabIndex = 1;
+            button1.Text = "Decision";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(351, 169);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(113, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveBorder;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+    }
+}
