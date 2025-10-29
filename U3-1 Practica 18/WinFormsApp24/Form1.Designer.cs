7e0c5ff49800737ffe60185ac6d4a7eab32789ac
karenaylinfdz <karenaylinfdz45@gmail.com>
2025-10-29 10:32:11 -0500

    Add files via upload

---
+++ U3-1 Practica 18/WinFormsApp24/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WinFormsApp24
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Arial", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(33, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(702, 19);
+            label1.TabIndex = 0;
+            label1.Text = "Diagrama que pido los 3 lados de un triangulo y diga si es escaleno,isoceles o equilatero";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.ABC;
+            pictureBox2.Location = new Point(566, 124);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(222, 278);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.trian;
+            pictureBox1.Location = new Point(274, 46);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(192, 80);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label2.ForeColor = SystemColors.ControlText;
+            label2.Location = new Point(33, 149);
+            label2.Name = "label2";
+            label2.Size = new Size(162, 22);
+            label2.TabIndex = 4;
+            label2.Text = "Longitud lado A:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label3.Location = new Point(31, 197);
+            label3.Name = "label3";
+            label3.Size = new Size(164, 22);
+            label3.TabIndex = 5;
+            label3.Text = "Longitud lado B:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 14.25F, FontStyle.Bold);
+            label4.Location = new Point(31, 245);
+            label4.Name = "label4";
+            label4.Size = new Size(164, 22);
+            label4.TabIndex = 6;
+            label4.Text = "Longitud lado C:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(205, 148);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(205, 197);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(205, 244);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button1.Location = new Point(31, 321);
+            button1.Name = "button1";
+            button1.Size = new Size(85, 38);
+            button1.TabIndex = 10;
+            button1.Text = "Decision ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button2.Location = new Point(205, 321);
+            button2.Name = "button2";
+            button2.Size = new Size(85, 38);
+            button2.TabIndex = 11;
+            button2.Text = "Borrar ";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 12F, FontStyle.Bold);
+            button3.Location = new Point(369, 321);
+            button3.Name = "button3";
+            button3.Size = new Size(85, 38);
+            button3.TabIndex = 12;
+            button3.Text = "Salir ";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(33, 395);
+            label5.Name = "label5";
+            label5.Size = new Size(153, 22);
+            label5.TabIndex = 13;
+            label5.Text = "Tipo Triangulo:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(205, 395);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label5;
+        private TextBox textBox4;
+    }
+}
